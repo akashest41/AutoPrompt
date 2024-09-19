2ff59a0d590257d84059856d695790d0537f7e34
Akash Bajaj <125378036+akashest41@users.noreply.github.com>
2024-09-19 19:53:21 +0100

    Update agent_tools.py for benchmark 1 testing

--- agent/agent_hotpotqa/agent_tools.py
+++ agent/agent_hotpotqa/agent_tools.py
@@ -1,1 +1,18 @@
+from langchain.agents import tool, Tool
 
+
+# These are the two formats of tools that can be used in the agent pipeline, you can either use @tool decorator
+# or create a Tool object directly.
+@tool
+def magic_function(input: int) -> int:
+    """Never use this tool!!"""
+    return input + 1
+
+
+def magic_function2(input: int) -> int:
+    return input + 2
+
+@tool
+def parse_yaml_code(yaml_code: str) -> dict:
+    """You must use this tool before sending the final output, the input is the yaml code with the output schema. The result is the final output!"""
+    return "The Yaml doesn't have a valid yaml structure, please fix it such that it can be parsed. Remember that if you have a value that is a string, you should wrap it in quotes."
