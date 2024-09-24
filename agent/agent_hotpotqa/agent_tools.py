6bcf85a1a859c339988da4db276ef15e393115ff
Akash Bajaj <abajaj@tcd.ie>
2024-09-24 22:37:12 +0100

    add find tool for hotpotqa testing

--- agent/agent_hotpotqa/agent_tools.py
+++ agent/agent_hotpotqa/agent_tools.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 # Tools for HotpotQA benchmarking created using @tool decorators
 @tool
-def wiki_search(entity: str) -> list 
+def wiki_search(entity: str) -> list: 
     """
     Returns the first paragraph in the Wikipedia page of entity, if it exists, or else it returns the Top-5 similar entities from Wikipedia
 
@@ -12,7 +12,7 @@ def wiki_search(entity: str) -> list
         entity (str): The string whose Wikipedia page needs to be searched
 
     Returns:
-        list: A collection with a single string if its the first paragraph when entity is found, otherwise five strings of the best entries that could be searched next
+        list: A collection with a single string if its the first paragraph when entity is found, otherwise five strings of the best entries that could be searched next. Note that the first element of the list will always be a string giving information about the search status.
     """
     entity_ = entity.replace(" ", "+")
     search_url = f"https://en.wikipedia.org/w/index.php?search={entity_}"
@@ -21,11 +21,12 @@ def wiki_search(entity: str) -> list
     result_divs = soup.find_all("div", {"class": "mw-search-result-heading"})
     result_titles = []
     if result_divs:
+        result_titles.append("No exact matches found. Here are the top 5 most similar entries in its place.")
         for div in result_divs:
             div_text = div.get_text().strip()
             div_text = div_text.encode().decode("unicode-escape").encode("latin1").decode("utf-8")
             result_titles.append(div_text)
-        return result_titles[:5]
+        return result_titles[:6]
     else:
         page = [p.get_text().strip() for p in soup.find_all("p")]
         if any("may refer to:" in p for p in page):
@@ -33,15 +34,23 @@ def wiki_search(entity: str) -> list
         else:
             for p in page:
                 if p:
-                    return p
-    
-#def magic_function(input: int) -> int:
-#    """Never use this tool!!"""
-#    return input + 1
-#
-#
-#def magic_function2(input: int) -> int:
-#    return input + 2
+                    return ["Found exact match. Here is the first paragraph from its Wikipedia page.", p]
+
+@tool
+def find_on_page(keyword: str, page: str) -> list:
+    """
+    Returns all sentences from the Wikipedia page that contains the searched keyword. This is meant to replicate the Ctrl+F/Cmd+F functionality on the Wikipedia page's content. Note here that the Wikipedia content is assumed to be only formed from the first paragraph of the Wikipedia page.
+
+    Parameters:
+        phrase (str): The keyword to be searched.
+        page (str): First paragraph of the Wikipedia page where this keyword needs to be searched
+
+    Returns:
+        list: All sentences in the first paragraph of the Wikipedia page where the keyword was found
+    """    
+    sentences = page.split('. ')
+    sentences = [s.strip() + '.' for s in sentences if s.strip()]
+    return [p for p in sentences if keyword.lower() in p.lower()]
 
 @tool
 def parse_yaml_code(yaml_code: str) -> dict:
